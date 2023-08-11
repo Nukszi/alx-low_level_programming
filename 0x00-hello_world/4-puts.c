cf54c6b5dbaf34715ca8f8175d55aa77dd1d985f
Michael <michaelselormey@gmail.com>
2023-08-10 22:42:09 -0700

    Task 4 puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main .a C program that prints
+ * retun: 0 (completed)
+  */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
