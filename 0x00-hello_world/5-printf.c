5f42cfcf72126971652d287026d5c43bf31e4bb0
Michael <michaelselormey@gmail.com>
2023-08-14 05:53:08 -0700

    Task 5 Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - the start of my C program
+ *
+ * description: a program to print out words
+ *
+ * Return: Always 0 (successful)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
