b7574049a74a8340f04e0ef00d093722da8f6d3f
Michael <michaelselormey@gmail.com>
2023-08-15 21:16:17 -0700

    Task 2 alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Program to print alphabets
+ *
+ * Description: this is a program that prints lower case alphabets with putchar
+ *
+ * Return: always 0 (successful)
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	a = 97;
+	while (a <= 122)
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
+
