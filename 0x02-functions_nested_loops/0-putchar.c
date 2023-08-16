191cc076b83d449fea91944bac59d2f58f71a67b
Michael <michaelselormey@gmail.com>
2023-08-15 18:29:26 -0700

    task 0 putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Print _putchar
+ *
+ * Description: this is a program to print out _putchar using putchar
+ *
+ * Return: always(0) successful
+ */
+int main(void)
+{
+putchar('_');
+putchar ('p');
+putchar ('u');
+putchar ('t');
+putchar ('c');
+putchar ('h');
+putchar ('a');
+putchar ('r');
+putchar ('\n');
+
+return (0);
+}
