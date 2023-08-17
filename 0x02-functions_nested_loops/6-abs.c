097af0af0fcdefa64e7821595b15057ad0d0369b
Michael <michaelselormey@gmail.com>
2023-08-16 20:03:56 -0700

    Task 6 Absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute
+ * @r: the variable to be checked
+ * Description: this program computes the absolute
+ *
+ * Return: (0)
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
