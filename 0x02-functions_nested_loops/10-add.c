6e74dd07c025d9ab973fbcc714b488f785b53689
Michael <michaelselormey@gmail.com>
2023-08-16 22:37:01 -0700

    Task 10 add 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds two numbers
+ *
+ * Description: this program adds two numbers
+ * @a: first number to add
+ * @b: second number to add
+ *
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
